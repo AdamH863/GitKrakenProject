be13c70b2477068b50ca22201407b8ca7d30c989
AdamH863 <10021448@sbstudents.org>
2024-10-30 13:52:30 -0400

    Joining Threads
    Each Thread is joined to the main Thread appropriately.

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -5,11 +5,11 @@ import java.util.concurrent.atomic.AtomicLong;
 public class Main
 {
 
-    public Main()
-    {
+    public Main()  {
         Thread[] threads = new Thread[1000];
         final AtomicLong sum = new AtomicLong(0L);
-        for (int x=0;x<1000;x++)
+        System.out.println("Creating Array of Threads");
+        for (int x=0;x<threads.length;x++)
         {
             threads[x] = new Thread(new Runnable() {
                 @Override
@@ -22,6 +22,20 @@ public class Main
             });
             threads[x].start();
         }
+
+        System.out.println("Joining Threads");
+
+
+        for (int x=0;x<threads.length;x++)
+        {
+            try {
+                threads[x].join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        System.out.println(sum);
     }
 
     public static void main(String[] args)
